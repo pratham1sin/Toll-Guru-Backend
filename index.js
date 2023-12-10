fa8bbbb14d3a9b59cbdd5a6668349adecb59c27f
“pratham <spratham906@gmail.com>
2023-12-10 22:57:53 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+
+  app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.status(204).end();
+  });
+
+  app.use(express.json())
+  app.use(express.urlencoded({extended:true}))
+  
+
+
+
+
+app.post('/origin-destination-waypoints', async (req, res) => {
+  const {From, To}=req.body
+  const from={"address": From}
+  const to={"address":To}
+      try {
+        const tollGuruResponse = await fetch('https://apis.tollguru.com/toll/v2/origin-destination-waypoints', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'Ln4NFdFDDntLMq7dpPTrMFMDr6H4N48J',
+              },
+            body: JSON.stringify({from, to})
+          });
+  
+      const tollGuruData = await tollGuruResponse.json();
+      res.json(tollGuruData);
+    } catch (error) {
+      console.error('Error during proxy request:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+// Your routes and other middleware here...
+
+app.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
